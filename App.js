3c1ee8b5a1f862d711c8b86d0fda65807cc3d639
Ana <alcvlara@sga.pucminas.br>
2019-02-13 19:27:34 -0200

    Creating main

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+// import React, { Component } from "react";
+// import { StyleSheet, Text, View } from "react-native";
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Oii</Text>
-      </View>
-    );
-  }
-}
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Oii</Text>
+//       </View>
+//     );
+//   }
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center"
+//   }
+// });
+
+import React from "react";
+import Routes from "./src/routes";
+
+const App = () => <Routes />;
+
+export default App;
